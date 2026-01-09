05f77a912bf033143dffa3476610f45a7eb7bd40
Backgold1804 <grustar02@gmail.com>
2026-01-09 15:14:00 +0900

    Render 배포를 위한 설정

--- backend/hugging_project/settings.py
+++ backend/hugging_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iqwjw414&+2=vn0p^mj8@i^ngk041wgjxw_cw3zm6+8jovjca9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1']
 
 
 # Application definition
