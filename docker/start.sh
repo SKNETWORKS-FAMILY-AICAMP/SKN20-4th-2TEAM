78fe0f2d4fa8163ee686d932f69fa384a1c4e9d0
Backgold1804 <grustar02@gmail.com>
2026-01-09 14:07:04 +0900

    docker 수정 중

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 set -e
+
+# Django 정적 파일을 수집합니다.
+echo "Collecting static files..."
+python backend/manage.py collectstatic --noinput
+
+# Nginx와 Gunicorn을 실행합니다.
 service nginx start
 cd backend
 gunicorn --workers 3 --bind 0.0.0.0:8000 hugging_project.wsgi:application
